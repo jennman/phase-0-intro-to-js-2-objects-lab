6ef85ae12c8ebd4244c7d93cc812582ffd895e59
Jennifer Man <jenniferman702@gmail.com>
2022-09-21 21:40:50 -0400

    commit tree 7704783bedae18f2bb6e5b6ef531c04903e560ae

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {};
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newObject = {...employee};
+    newObject[key] = value ;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    const newObject = employee;
+    newObject[key] = value;
+    return newObject;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = {...key};
+    delete deleteFromEmployeeByKey[key];
+    return newObject;
+
+}
